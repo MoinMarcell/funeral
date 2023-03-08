2936f4e456dd12bd19487a85f4fa523adc5a02b8
MoinMarcell <marcell.dechant@proton.me>
2023-03-09 00:21:13 +0100

    Add method to create dead person

--- frontend/src/service/apiService.ts
+++ frontend/src/service/apiService.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
+import {DeadPerson} from "../models/DeadPerson";
 
 const BASE_URI: string = "/api/dead-persons";
 
 export async function getAllDeadPersons() {
     return await axios.get(BASE_URI);
+}
+
+export async function createDeadPerson(deadPerson: DeadPerson) {
+    return await axios.post(BASE_URI, deadPerson);
 }

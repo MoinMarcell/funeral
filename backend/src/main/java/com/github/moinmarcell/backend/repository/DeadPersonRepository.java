1e2d97e4a94e1c628d3fb4ffc1df5305e957897b
MoinMarcell <marcell.dechant@proton.me>
2023-03-08 20:27:32 +0100

    Add repository annotation

--- backend/src/main/java/com/github/moinmarcell/backend/repository/DeadPersonRepository.java
+++ backend/src/main/java/com/github/moinmarcell/backend/repository/DeadPersonRepository.java
@@ -2,6 +2,8 @@ package com.github.moinmarcell.backend.repository;
 
 import com.github.moinmarcell.backend.model.DeadPerson;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeadPersonRepository extends MongoRepository<DeadPerson, String> {
 }

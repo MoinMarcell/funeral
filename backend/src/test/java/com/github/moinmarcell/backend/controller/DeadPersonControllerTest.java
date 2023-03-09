dcfe7e422b4706439d813b0551b839ce8ab4d737
Marcell Dechant <marcell.dechant@proton.me>
2023-03-09 23:37:00 +0100

    Add endpoint to update dead person & add test

--- backend/src/test/java/com/github/moinmarcell/backend/controller/DeadPersonControllerTest.java
+++ backend/src/test/java/com/github/moinmarcell/backend/controller/DeadPersonControllerTest.java
@@ -13,8 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -103,5 +102,99 @@ class DeadPersonControllerTest {
         DeadPerson deadPerson = objectMapper.readValue(jsonObj, DeadPerson.class);
         mockMvc.perform(get("/api/dead-persons/" + deadPerson.id()).with(csrf()))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser
+    void getDeadPersonById_whenDeadPersonNotExist_thenReturnStatusNotFound() throws Exception {
+        mockMvc.perform(get("/api/dead-persons/1").with(csrf()))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser
+    void updateDeadPerson_whenDeadPersonExist_thenStatusOkAndReturnUpdatedDeadPerson() throws Exception {
+        MvcResult result = mockMvc.perform(post("/api/dead-persons")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                  "firstName": "firstName",
+                                  "lastName": "lastName",
+                                  "dateOfBirth": "dateOfBirth",
+                                  "dateOfDeath": "dateOfDeath",
+                                  "placeOfBirth": "placeOfBirth",
+                                  "placeOfDeath": "placeOfDeath",
+                                  "street": "street",
+                                  "houseNumber": "houseNumber",
+                                  "zipCode": "zipCode",
+                                  "city": "city",
+                                  "country": "country"
+                                }
+                                """)
+                        .with(csrf()))
+                .andReturn();
+        String jsonObj = result.getResponse().getContentAsString();
+        DeadPerson deadPerson = objectMapper.readValue(jsonObj, DeadPerson.class);
+        mockMvc.perform(put("/api/dead-persons/" + deadPerson.id())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                  "firstName": "firstName2",
+                                  "lastName": "lastName2",
+                                  "dateOfBirth": "dateOfBirth2",
+                                  "dateOfDeath": "dateOfDeath2",
+                                  "placeOfBirth": "placeOfBirth2",
+                                  "placeOfDeath": "placeOfDeath2",
+                                  "street": "street2",
+                                  "houseNumber": "houseNumber2",
+                                  "zipCode": "zipCode2",
+                                  "city": "city2",
+                                  "country": "country2"
+                                }
+                                """)
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                          "firstName": "firstName2",
+                          "lastName": "lastName2",
+                          "dateOfBirth": "dateOfBirth2",
+                          "dateOfDeath": "dateOfDeath2",
+                          "placeOfBirth": "placeOfBirth2",
+                          "placeOfDeath": "placeOfDeath2",
+                          "street": "street2",
+                          "houseNumber": "houseNumber2",
+                          "zipCode": "zipCode2",
+                          "city": "city2",
+                          "country": "country2"
+                        }
+                        """));
+    }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser
+    void updateDeadPerson_whenDeadPersonNotExist_thenStatusNotFound() throws Exception {
+        mockMvc.perform(put("/api/dead-persons/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                  "firstName": "firstName2",
+                                  "lastName": "lastName2",
+                                  "dateOfBirth": "dateOfBirth2",
+                                  "dateOfDeath": "dateOfDeath2",
+                                  "placeOfBirth": "placeOfBirth2",
+                                  "placeOfDeath": "placeOfDeath2",
+                                  "street": "street2",
+                                  "houseNumber": "houseNumber2",
+                                  "zipCode": "zipCode2",
+                                  "city": "city2",
+                                  "country": "country2"
+                                }
+                                """)
+                        .with(csrf()))
+                .andExpect(status().isNotFound());
     }
 }

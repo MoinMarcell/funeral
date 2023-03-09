db420cfff193fc9d7a8588358abcb43941659c42
MoinMarcell <marcell.dechant@proton.me>
2023-03-09 17:30:28 +0100

    Add routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import {DeadPersonApp} from "./components/deadPersons/DeadPersonApp";
+import {Route, Routes} from "react-router-dom";
+import DeadPersonDetailsPage from "./components/deadPersons/DeadPersonDetailsPage";
 
 function App() {
     return (
-        <div className="App">
-            <DeadPersonApp/>
-        </div>
+        <Routes>
+            <Route path="/" element={<DeadPersonApp/>}/>
+            <Route path={"/dead-persons"} element={<DeadPersonApp/>}/>
+            <Route path={"/:id"} element={<DeadPersonDetailsPage/>}/>
+        </Routes>
     );
 }
 
